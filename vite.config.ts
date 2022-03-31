528941d14eb8cab11712f67cf68038ebac943c62
lopo <lopo@zju.edu.cn>
2022-03-31 13:17:01 +0800

    chore: change config

--- vite.config.ts
+++ vite.config.ts
@@ -26,13 +26,6 @@ export default defineConfig({
         port: 8899
     },
     build: {
-        // 子项目必须打包成库模式, 且入口文件名为 [main.js] 供主项目调用
-        lib: {
-            entry: resolve(__dirname, './src/adaptor.tsx'),
-            formats: [ 'es' ],
-            fileName: () => 'adaptor.js'
-        },
-        outDir: "../../dist/packages/flow-chart",
-        emptyOutDir: false
+        outDir: './dist'
     }
 })
