90fb5bc4ed3d0161051791d8d394abcd8283fac2
lopo <lopo@zju.edu.cn>
2022-04-07 16:25:48 +0800

    feat: add a nav-bar instead of default bar

---
+++ src/views/App.tsx
@@ -0,0 +1,38 @@
+import FlowChart from "@flowChart/views/FlowChart";
+import Styles from "@flowChart/styles/navbar.module.scss"
+
+export const App = () => {
+    const doOperate = (type: 'min' | 'max' | 'close') => {
+
+    }
+
+    return (
+        <div style={ {
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        } }>
+            <div className={Styles.app}>
+                <div className={Styles.logo}>
+                    <i>i</i>`<b>m</b> <u>l</u><b>o</b>g<b>o</b>
+                </div>
+                <div className={Styles.btnGroup}>
+                    <div className={Styles.btn}><i className="pi pi-window-minimize"/></div>
+                    <div className={Styles.btn}><i className="pi pi-window-maximize"/></div>
+                    <div className={Styles.btn}><i className="pi pi-times"/></div>
+                </div>
+            </div>
+            <div style={ {
+                position: 'relative',
+                width: '100%',
+                height: 'calc(100% - 40px)',
+            } }>
+                <FlowChart/>
+            </div>
+        </div>
+    )
+}
