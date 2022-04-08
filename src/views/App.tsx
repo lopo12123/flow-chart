fa6a70e1a3c1ab05b078fb1a195132278dc52180
lopo <lopo@zju.edu.cn>
2022-04-08 10:11:14 +0800

    feat: add f12 event and refresh

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,26 @@
 import FlowChart from "@flowChart/views/FlowChart";
 import Styles from "@flowChart/styles/navbar.module.scss"
+import { useEffect } from "react";
 
 export const App = () => {
     // @ts-ignore
-    const sendOperateIpc = window.customApis?.sendOperateIpc ?? (() => {})
+    const sendOperateIpc = window.customApis?.sendOperateIpc ?? (() => {
+    })
+
+    const doRefresh = () => {
+        window.location.reload()
+    }
+
+    useEffect(() => {
+        const openDevTool = (e: KeyboardEvent) => {
+            if(e.code === 'F12') sendOperateIpc('devTool')
+        }
+        window.addEventListener('keydown', openDevTool)
+
+        return () => {
+            window.removeEventListener('keydown', openDevTool)
+        }
+    }, [])
 
     return (
         <div style={ {
@@ -15,14 +32,18 @@ export const App = () => {
             alignItems: 'center',
             justifyContent: 'space-between'
         } }>
-            <div className={Styles.app}>
-                <div className={Styles.logo}>
+            <div className={ Styles.app }>
+                <div className={ Styles.logo }>
                     <i>i</i>`<b>m</b> <u>l</u><b>o</b>g<b>o</b>
                 </div>
-                <div className={Styles.btnGroup}>
-                    <div className={Styles.btn} onClick={() => sendOperateIpc('min')}><i className="pi pi-window-minimize"/></div>
-                    <div className={Styles.btn} onClick={() => sendOperateIpc('max')}><i className="pi pi-window-maximize"/></div>
-                    <div className={Styles.btn} onClick={() => sendOperateIpc('close')}><i className="pi pi-times"/></div>
+                <div className={ Styles.btnGroup }>
+                    <div className={ Styles.btn } onClick={ () => sendOperateIpc('min') }><i
+                        className="pi pi-window-minimize"/></div>
+                    <div className={ Styles.btn } onClick={ () => sendOperateIpc('max') }><i
+                        className="pi pi-window-maximize"/></div>
+                    <div className={ Styles.btn } onClick={ doRefresh }><i className="pi pi-sync"/></div>
+                    <div className={ Styles.btn } onClick={ () => sendOperateIpc('close') }><i className="pi pi-times"/>
+                    </div>
                 </div>
             </div>
             <div style={ {
